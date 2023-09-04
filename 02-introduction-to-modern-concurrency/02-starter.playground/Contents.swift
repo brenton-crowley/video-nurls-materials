283114fd1851afe18f9cf8fbdd43e618197ee6c9
Brent Crowley <brent.crowley@seventhbeam.com>
2023-09-04 15:48:21 +1000

    complete introduction to modern concurrency

--- 02-introduction-to-modern-concurrency/02-starter.playground/Contents.swift
+++ 02-introduction-to-modern-concurrency/02-starter.playground/Contents.swift
@@ -31,3 +31,25 @@
 /// THE SOFTWARE.
 
 import SwiftUI
+
+func fetchDomains()  async throws -> [Domain] {
+    let url = URL(string: "http://api.raywenderlich.com/api/domains")!
+    let (data, _) = try await URLSession.shared.data(from: url)
+    
+    return try JSONDecoder().decode(Domains.self, from: data).data
+}
+
+Task {
+    do {
+        let domains = try await fetchDomains()
+        
+        for (index, domain) in domains.enumerated() {
+            let attr = domain.attributes
+            print("\(index + 1) \(attr.name): \(attr.description) - \(attr.level)")
+        }
+    } catch let error {
+        print(error)
+    }
+}
+
+
